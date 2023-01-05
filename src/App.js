f68059fe6673d5a00bca7347ac43873270a1446e
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-04 23:00:56 -0600

    updating code for calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState, useEffect} from 'react'
+import { NumberFormatBase } from 'react-number-format';
 
 function App() {
 
@@ -10,35 +11,100 @@ const [operator, setOperator] = useState(null)
 const [total, setTotal] = useState(false)
 
 const inputNum = e => {
+  if (curState.includes(".") && e.target.innerText === ".") return
+  
+  if (total) {
+    setPreState("")
+  }
 
-}
+  curState ? setCurState((pre) => pre + e.target.innerText) : setCurState(e.target.innerText);
+  setTotal(false);
+};
+
+useEffect(() => {
+  setInput(curState)
+},[curState])
+
+useEffect(() => {
+  setInput("0")
+}, [])
 
 const operatorType = e => {
-
-}
+  setTotal(false)
+  setOperator(e.target.innerText)
+  if (curState === "") return
+  if (preState !== "") {
+    equals()
+  }else {
+    setPreState(curState);
+    setCurState("");
+  }
+};
 
 const  equals = e => {
-
+  if (e?.target.innerText === "="){
+    setTotal(true)
+};
+let cal
+switch (operator) {
+  case "/":
+    cal = String(parseFloat(preState) / parseFloat(curState));
+    break;
+  case "+":
+    cal = String(parseFloat(preState) + parseFloat(curState));
+    break;
+  case "X":
+    cal = String(parseFloat(preState) * parseFloat(curState));
+    break;
+  case "-":
+    cal = String(parseFloat(preState) - parseFloat(curState));
+    break;
+    default: 
+      return
+}
+setInput("")
+setPreState(cal)
+setCurState("")
 }
 
 const minusPlus = () => {
-
-}
+  if (curState.charAt(0) === "-") {
+    setCurState(curState.substring(1))
+  } else {
+    setCurState("-"+curState)
+  }
+};
 
 const percent = () => {
+  preState ? setCurState(String(parseFloat(curState) / 100 * preState)) : setCurState(String(parseFloat(curState) / 100));
 
-}
+};
 
 const reset = () => {
-  
+  setPreState("")
+  setCurState("")
+  setInput("0");
 }
-
 
 return (
    <>
    <div className="container">
     <div className="page-wrapper">
-      <div className="screen"></div>
+      <div className="screen">
+        {input !== "" || input === "0" ? (
+          <NumberFormatBase
+            value={input}
+            displayType={"text"}
+            thousandSeparator={true}
+          />
+          ) : (
+            <NumberFormatBase
+            value={preState}
+            displayType={"text"}
+            thousandSeparator={true}
+          />
+        )}
+      </div>
         <div className="btn light-gray" onClick={reset}>AC</div>
         <div className="btn light-gray" onClick={percent}>%</div>
         <div className="btn light-gray" onClick={minusPlus}>+/-</div>
@@ -55,11 +121,11 @@ return (
         <div className="btn" onClick={inputNum}>2</div>
         <div className="btn" onClick={inputNum}>3</div>
         <div className="btn orange" onClick={operatorType}>-</div>
-        <div className="btn" onClick={inputNum}>0</div>
+        <div className="btn zero" onClick={inputNum}>0</div>
         <div className="btn" onClick={inputNum}>.</div>
         <div className="btn" onClick={equals}>=</div>
-      </div> {/* END OF CONTAINER */} 
-    </div>{/* END OF PAGE WRAPPER */}
+      </div>{/* END OF PAGE WRAPPER */}
+    </div> {/* END OF CONTAINER */} 
    </>
   );
 }
