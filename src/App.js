178e90ad5d6b42df3744f3af39fce029b1891d3c
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-04 21:41:29 -0600

    updating css

--- src/App.js
+++ src/App.js
@@ -2,34 +2,62 @@ import './App.css';
 import {useState, useEffect} from 'react'
 
 function App() {
-  return (
+
+const [preState, setPreState] = useState("")
+const [curState, setCurState] = useState("")
+const [input, setInput] = useState("0")
+const [operator, setOperator] = useState(null)
+const [total, setTotal] = useState(false)
+
+const inputNum = e => {
+
+}
+
+const operatorType = e => {
+
+}
+
+const  equals = e => {
+
+}
+
+const minusPlus = () => {
+
+}
+
+const percent = () => {
+
+}
+
+const reset = () => {
+  
+}
+
+
+return (
    <>
+   <div className="container">
     <div className="page-wrapper">
-      <div className="container">
-          <div className="row">
-              <div className="col">
-                <div className="screen"></div>
-                  <div className="btn light-gray" onClick={reset}>AC</div>
-                  <div className="btn light-gray" onClick={percent}>%</div>
-                  <div className="btn light-gray" onClick={minusPlus}>+/-</div>
-                  <div className="btn orange" onClick={operatorType}>/</div>
-                  <div className="btn" onClick={inputNum}>7</div>
-                  <div className="btn" onClick={inputNum}>8</div>
-                  <div className="btn" onClick={inputNum}>9</div>
-                  <div className="btn orange" onClick={operatorType}>X</div>
-                  <div className="btn" onClick={inputNum}>4</div>
-                  <div className="btn" onClick={inputNum}>5</div>
-                  <div className="btn" onClick={inputNum}>6</div>
-                  <div className="btn orange" onClick={operatorType}>+</div>
-                  <div className="btn" onClick={inputNum}>1</div>
-                  <div className="btn" onClick={inputNum}>2</div>
-                  <div className="btn" onClick={inputNum}>3</div>
-                  <div className="btn orange" onClick={operatorType}>-</div>
-                  <div className="btn" onClick={inputNum}>0</div>
-                  <div className="btn" onClick={inputNum}>.</div>
-                  <div className="btn" onClick={equals}>=</div>
-              </div>{/* END OF COL */}
-          </div>{/* END OF ROW */}
+      <div className="screen"></div>
+        <div className="btn light-gray" onClick={reset}>AC</div>
+        <div className="btn light-gray" onClick={percent}>%</div>
+        <div className="btn light-gray" onClick={minusPlus}>+/-</div>
+        <div className="btn orange" onClick={operatorType}>/</div>
+        <div className="btn" onClick={inputNum}>7</div>
+        <div className="btn" onClick={inputNum}>8</div>
+        <div className="btn" onClick={inputNum}>9</div>
+        <div className="btn orange" onClick={operatorType}>X</div>
+        <div className="btn" onClick={inputNum}>4</div>
+        <div className="btn" onClick={inputNum}>5</div>
+        <div className="btn" onClick={inputNum}>6</div>
+        <div className="btn orange" onClick={operatorType}>+</div>
+        <div className="btn" onClick={inputNum}>1</div>
+        <div className="btn" onClick={inputNum}>2</div>
+        <div className="btn" onClick={inputNum}>3</div>
+        <div className="btn orange" onClick={operatorType}>-</div>
+        <div className="btn" onClick={inputNum}>0</div>
+        <div className="btn" onClick={inputNum}>.</div>
+        <div className="btn" onClick={equals}>=</div>
       </div> {/* END OF CONTAINER */} 
     </div>{/* END OF PAGE WRAPPER */}
    </>
