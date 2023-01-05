f5aa46a019ba186d98251f13f03087c4564a9fa7
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-04 21:02:18 -0600

    adding buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <div className="page-wrapper">
+      <div className="container">
+          <div className="row">
+              <div className="col">
+                <div className="screen"></div>
+                  <div className="btn light-gray" onClick={reset}>AC</div>
+                  <div className="btn light-gray" onClick={percent}>%</div>
+                  <div className="btn light-gray" onClick={minusPlus}>+/-</div>
+                  <div className="btn orange" onClick={operatorType}>/</div>
+                  <div className="btn" onClick={inputNum}>7</div>
+                  <div className="btn" onClick={inputNum}>8</div>
+                  <div className="btn" onClick={inputNum}>9</div>
+                  <div className="btn orange" onClick={operatorType}>X</div>
+                  <div className="btn" onClick={inputNum}>4</div>
+                  <div className="btn" onClick={inputNum}>5</div>
+                  <div className="btn" onClick={inputNum}>6</div>
+                  <div className="btn orange" onClick={operatorType}>+</div>
+                  <div className="btn" onClick={inputNum}>1</div>
+                  <div className="btn" onClick={inputNum}>2</div>
+                  <div className="btn" onClick={inputNum}>3</div>
+                  <div className="btn orange" onClick={operatorType}>-</div>
+                  <div className="btn" onClick={inputNum}>0</div>
+                  <div className="btn" onClick={inputNum}>.</div>
+                  <div className="btn" onClick={equals}>=</div>
+              </div>{/* END OF COL */}
+          </div>{/* END OF ROW */}
+      </div> {/* END OF CONTAINER */} 
+    </div>{/* END OF PAGE WRAPPER */}
+   </>
   );
 }
 
